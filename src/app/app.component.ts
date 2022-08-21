3cdf8cc2477b1c77fdb337f254b4f1ebf7da706a
Hieu Khuong <khuonghuyhieu0705@gmail.com>
2022-08-22 01:19:57 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1 [class.with-border]="hiden">Wellcome My Home {{ hiden ? title : '' }}</h1>
+    <button (click)="onButtonClick()">Click {{hiden ? "Hiden" : "Show"}}</button>`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = 'Hiếu';
+  hiden = true;
+  onButtonClick() {
+    this.hiden = !this.hiden;
+  }
 }
